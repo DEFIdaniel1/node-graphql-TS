b21cc6dc7f836b3c0d8bde61915f61aaaf435747
danala <greatprotectionwallet@gmail.com>
2022-10-29 15:48:37 -0600

    converting files to typescript

--- config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-    jwtSecret: process.env.JWT_SECRET,
-    mongoDbPassword: process.env.MONGO_DB_PASSWORD,
-    mongoUser: process.env.MONGO_USER,
-    port: process.env.PORT,
-}
