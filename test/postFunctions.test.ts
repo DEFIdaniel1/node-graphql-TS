649ac39cfd63d0981ee6b2cc278fcbe318bbcf63
danala <greatprotectionwallet@gmail.com>
2022-10-31 14:06:06 -0600

    postFunctions tests updated: update and delete tests complete and successful

--- test/postFunctions.test.ts
+++ test/postFunctions.test.ts
@@ -1,4 +1,5 @@
 import request from 'supertest'
+import { idText } from 'typescript'
 import app from '../app'
 import { Token } from '../models/custom'
 import { NewUserInput } from './testTypes'
@@ -52,7 +53,6 @@ describe('Post Functions', () => {
             .set('Content-Type', 'application/json')
         authToken = loginResponse.body.data.login.token
         loginUserId = loginResponse.body.data.login.userId
-        console.log('loginUserID ' + loginUserId)
     })
     describe('Create a new post', () => {
         const postData = {
@@ -78,6 +78,10 @@ describe('Post Functions', () => {
                 `,
         }
         let createPostResponse: any
+        let postId: string
+        let updatePostResponse: any
+
+        let deletePostResponse: any
 
         beforeAll(async () => {
             createPostResponse = await request(app)
@@ -90,23 +94,92 @@ describe('Post Functions', () => {
             expect(createPostResponse.status).toBe(200)
         })
         it('Should receive response body with title, content, imageUrl', () => {
-            const { title, content, imageUrl } =
+            const { _id, title, content, imageUrl } =
                 createPostResponse.body.data.createPost
             expect([title, content, imageUrl]).toEqual([
                 postData.title,
                 postData.content,
                 postData.imageUrl,
             ])
+            postId = _id
+        })
+        describe('Update post', () => {
+            const postUpdate = {
+                title: 'Some new and exciting title!',
+                content: 'Wow, update content test',
+                imageUrl: 'An updated URL',
+            } as const
+
+            beforeAll(async () => {
+                const updatePostQuery = {
+                    query: `
+                        mutation {
+                                updatePost(id: "${postId}", postInput: {title: "${postUpdate.title}", content: "${postUpdate.content}", imageUrl: "${postUpdate.imageUrl}"}) {
+                                    _id
+                                    title
+                                    content
+                                    imageUrl
+                                    creator {
+                                        name
+                                    }
+                                    createdAt
+                                    }
+                                }
+                            `,
+                }
+                updatePostResponse = await request(app)
+                    .post('/graphql')
+                    .send(JSON.stringify(updatePostQuery))
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', `Bearer ${authToken}`)
+            })
+            it('Returns 200 status', () => {
+                expect(updatePostResponse.status).toBe(200)
+            })
+            it('Should receive response body with UPDATED title, content, imageUrl', () => {
+                const {
+                    title: newTitle,
+                    content: newContent,
+                    imageUrl: newImageUrl,
+                } = updatePostResponse.body.data.updatePost
+                expect([
+                    postUpdate.title,
+                    postUpdate.content,
+                    postUpdate.imageUrl,
+                ]).toEqual([newTitle, newContent, newImageUrl])
+            })
+        })
+        describe('Delete post', () => {
+            beforeAll(async () => {
+                const deletePostQuery = {
+                    query: `
+                mutation {
+                  deletePost(id: "${postId}")
+                }
+              `,
+                }
+                deletePostResponse = await request(app)
+                    .post('/graphql')
+                    .send(JSON.stringify(deletePostQuery))
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', `Bearer ${authToken}`)
+            })
+            it('Should receive 200 success response', () => {
+                expect(deletePostResponse.status).toBe(200)
+            })
+            it('Should return true in response body', () => {
+                expect(deletePostResponse.body.data.deletePost).toBe(true)
+            })
         })
     })
     afterAll(async () => {
         // Delete new user
         const deleteUserQuery = {
             query: `
-                mutation {
-                    deleteUser(id: "${loginUserId}")
-                }
-                `,
+                    mutation {
+                        deleteUser(id: "${loginUserId}")
+                    }
+                    `,
         }
         await request(app)
             .post('/graphql')
